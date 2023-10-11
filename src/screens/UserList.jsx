bc28e962e841ede386a62c71a7515fa1fd31b2b6
AUGUSTO DE OLIVEIRA BASSO <00001095593158sp@aluno.educacao.sp.gov.br>
2023-10-10 21:42:46 -0300

    fiz o get

---
+++ src/screens/UserList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import EditUser from './EditUser'
+import AddUser from './AddUser'
+import './UserList.css'
+import axios from 'axios'
+
+export default function UserList() {
+    const [users, setUsers]=useState([]);
+
+    const pegaUser = async() =>{
+        try {
+            const resposta = await axios.get('http://127.0.0.1:8000/usuarios')
+            setUsers(resposta.data)
+            console.log(users)
+        } catch (erros) {
+            console.log(erros) 
+        }
+    }
+
+    useEffect(()=>{
+        pegaUser();
+    }, [])
+
+  return (
+    <div>
+        <h1> lista user</h1>
+        <ul>
+            {users.map((user)=>(
+            <>
+            <li>{user.nome}</li>
+            <li>{user.email}</li>
+            <li>{user.saldo}</li>
+            </>
+            ))}
+            
+        </ul>
+    </div>
+  )
+}
